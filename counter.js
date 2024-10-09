4931befe9e4edffcd477f3f152a3e2ecd9eacccc
LigerCode3443 <161658640+LigerCode3443@users.noreply.github.com>
2024-10-09 23:02:51 +0300

    add src folder

--- counter.js
+++ counter.js
@@ -1,10 +1,10 @@
-import img1 from "./image/img_quiz/1.png";
-import img2 from "./image/img_quiz/2.png";
-import img3 from "./image/img_quiz/3.png";
-import img4 from "./image/img_quiz/4.png";
-import img5 from "./image/img_quiz/5.png";
-import img6 from "./image/img_quiz/6.png";
-import img7 from "./image/img_quiz/7.png";
+import img1 from "./src/image/img_quiz/1.png";
+import img2 from "./src/image/img_quiz/2.png";
+import img3 from "./src/image/img_quiz/3.png";
+import img4 from "./src/image/img_quiz/4.png";
+import img5 from "./src/image/img_quiz/5.png";
+import img6 from "./src/image/img_quiz/6.png";
+import img7 from "./src/image/img_quiz/7.png";
 
 export const images = {
   img1,
